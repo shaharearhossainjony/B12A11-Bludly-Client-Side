77dd1627eccdd304de6d0786896da2bb91c27a13
shaharearhossainjony <shaharearhossainjony@gmail.com>
2025-12-16 05:07:14 +0600

    Bludly project created and tailwind css, daisy ui, react router install and setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      
+      <h1 className='text-red-500'>Bludly - Donate Blood, Save Life</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
